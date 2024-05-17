f685b2c01e4c1dd40b2bde583448b6439df1d22c
Tairan Ding <tairan.ding@ucdconnect.ie>
2024-05-17 11:09:17 +0800

    update assignment solution

--- assignment/solution.c
+++ assignment/solution.c
@@ -51,7 +51,6 @@
 #include <stdlib.h>
 
 #include <mpi.h>
-#include <math.h>
 
 #define ROOT_PROCESS 0
 
@@ -67,77 +66,96 @@ static int parse_order(int ** restrict item, int ** restrict quantity);
 #define FEN(centi_rmb) ((int) (centi_rmb % 100))
 
 
+int my_pow(int base, int y) {
+    int result = 1;
+    for (int i = 0; i < y; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+// since x will be the power of 2 and x != 1, so x >= 2
+int my_log2(int x) {
+    // if the input x is less than 0, return -1 (wrong code)
+    int result = -1;
+    while (x > 0){
+        x>>=1;
+        result++;
+    }
+    return result;
+}
+
 int main(int argc, char* argv[]) {
-    // 0. Initialize
-    int size, rank;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	// 0. Initialize
+	int size, rank;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Prices per each item on the menu, in fēn:
-    int menu_prices[ITEMS_ON_MENU] = {0};
+	// Prices per each item on the menu, in fēn:
+	int menu_prices[ITEMS_ON_MENU] = {0};
 
-    // 1. Parse the menu
-    if (rank == ROOT_PROCESS) {
-        // Parse menu ONLY on root process!
-        parse_menu(menu_prices);
-    }
+	// 1. Parse the menu
+	if (rank == ROOT_PROCESS) {
+		// Parse menu ONLY on root process!
+		parse_menu(menu_prices);
+	}
 
-    // 2. Share the prices with the other processes
-    MPI_Bcast(menu_prices, ITEMS_ON_MENU, MPI_INT,
-              ROOT_PROCESS, MPI_COMM_WORLD);
+	// 2. Share the prices with the other processes
+	MPI_Bcast(menu_prices, ITEMS_ON_MENU, MPI_INT,
+			ROOT_PROCESS, MPI_COMM_WORLD);
 
-    // 3. Parse the order
-    // The order is made of two **parallel arrays**:
-    int* all_items = NULL;
-    int* all_quantities = NULL;
-    int size_of_order = -1;
-    if (rank == ROOT_PROCESS) {
-        // Parse and allocate memory for the full order **ONLY** on
-        // the root process:
-        size_of_order = parse_order(&all_items, &all_quantities);
-        assert(all_items != NULL);
-        assert(all_quantities != NULL);
-    }
+	// 3. Parse the order
+	// The order is made of two **parallel arrays**:
+	int* all_items = NULL;
+	int* all_quantities = NULL;
+	int size_of_order = -1;
+	if (rank == ROOT_PROCESS) {
+		// Parse and allocate memory for the full order **ONLY** on
+		// the root process:
+		size_of_order = parse_order(&all_items, &all_quantities);
+		assert(all_items != NULL);
+		assert(all_quantities != NULL);
+	}
 
-    // 3.5. Send the order size to all processes.
-    MPI_Bcast(&size_of_order, 1, MPI_INT, ROOT_PROCESS, MPI_COMM_WORLD);
-    assert(size_of_order >= 0);
+	// 3.5. Send the order size to all processes.
+	MPI_Bcast(&size_of_order, 1, MPI_INT, ROOT_PROCESS, MPI_COMM_WORLD);
+	assert(size_of_order >= 0);
 
-    // 4. Allocate memory for the segments
-    // **EVERY** node has its own segment arrays!
-    const int segment_size = size_of_order / size;
-    int *segment_items = calloc(segment_size, sizeof(int));
-    int *segment_quantities = calloc(segment_size, sizeof(int));
-    assert(segment_items != NULL);
-    assert(segment_quantities != NULL);
+	// 4. Allocate memory for the segments
+	// **EVERY** node has its own segment arrays!
+	const int segment_size = size_of_order / size;
+	int *segment_items = calloc(segment_size, sizeof(int));
+	int *segment_quantities = calloc(segment_size, sizeof(int));
+	assert(segment_items != NULL);
+	assert(segment_quantities != NULL);
 
-    // 5. Scatter both arrays from the root node to all other processes
-    //    (including the root node itself!)
-    MPI_Scatter(
-            all_items, segment_size, MPI_INT,
-            segment_items, segment_size, MPI_INT,
-            ROOT_PROCESS, MPI_COMM_WORLD
-    );
-    MPI_Scatter(
-            all_quantities, segment_size, MPI_INT,
-            segment_quantities, segment_size, MPI_INT,
-            ROOT_PROCESS, MPI_COMM_WORLD
-    );
+	// 5. Scatter both arrays from the root node to all other processes
+	//    (including the root node itself!)
+	MPI_Scatter(
+		all_items, segment_size, MPI_INT,
+		segment_items, segment_size, MPI_INT,
+		ROOT_PROCESS, MPI_COMM_WORLD
+	);
+	MPI_Scatter(
+		all_quantities, segment_size, MPI_INT,
+		segment_quantities, segment_size, MPI_INT,
+		ROOT_PROCESS, MPI_COMM_WORLD
+	);
 
-    // 6. Each process can sum its own segment!
-    long cost_per_menu_item[ITEMS_ON_MENU] = {0};
-    for (int i = 0; i < segment_size; i++) {
-        int item = segment_items[i];
-        int qty = segment_quantities[i];
-        int price = menu_prices[item];
+	// 6. Each process can sum its own segment!
+	long cost_per_menu_item[ITEMS_ON_MENU] = {0};
+	for (int i = 0; i < segment_size; i++) {
+		int item = segment_items[i];
+		int qty = segment_quantities[i];
+		int price = menu_prices[item];
 
-        cost_per_menu_item[item] += price * qty;
-    }
+		cost_per_menu_item[item] += price * qty;
+	}
 
-    // 7. Get all the partial sums and place them on the root node.
-    //long total_cost_per_menu_item[ITEMS_ON_MENU] = {0};
-    // TODO: change this call to use point-to-point communication instead!
+	// 7. Get all the partial sums and place them on the root node.
+	//long total_cost_per_menu_item[ITEMS_ON_MENU] = {0};
+	// TODO: change this call to use point-to-point communication instead!
 //	MPI_Reduce(
 //		cost_per_menu_item, total_cost_per_menu_item,
 //		ITEMS_ON_MENU, MPI_LONG, MPI_SUM,
@@ -146,9 +164,9 @@ int main(int argc, char* argv[]) {
 
 
     long recvData[ITEMS_ON_MENU] = {0};
-    double power = log2(size);
+    int power = my_log2(size);
     for (long i = 1; i<=power; i++){
-        long involvedProcNumber = size/(pow(2,i));
+        long involvedProcNumber = size/(my_pow(2,i));
         if (rank+1 > involvedProcNumber){
             // cost_per_menu_item represents the overall cost (qty*price) inside the process
             MPI_Send(cost_per_menu_item, ITEMS_ON_MENU, MPI_LONG, rank-involvedProcNumber, 0, MPI_COMM_WORLD);
@@ -156,7 +174,7 @@ int main(int argc, char* argv[]) {
             MPI_Recv(recvData, ITEMS_ON_MENU, MPI_LONG, rank+involvedProcNumber, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             for (int ii = 0; ii<ITEMS_ON_MENU; ii++)
                 cost_per_menu_item[ii] += recvData[ii];
-        }
+            }
     }
 
 
@@ -173,91 +191,91 @@ int main(int argc, char* argv[]) {
 //    }
 
 
-    // NOTE: You can leave the remainder of this file unchanged:
-    // 8. Calculate the grand total on the root node!
-    if (rank == ROOT_PROCESS) {
-        long grand_total = 0;
-        for (int i = 0; i < ITEMS_ON_MENU; i++) {
-            long cost = cost_per_menu_item[i];
-            grand_total += cost;
-        }
-        printf(
-                "The bill comes out to ¥%ld.%02d\n",
-                YUAN(grand_total), FEN(grand_total)
-        );
-    }
+	// NOTE: You can leave the remainder of this file unchanged:
+	// 8. Calculate the grand total on the root node!
+	if (rank == ROOT_PROCESS) {
+		long grand_total = 0;
+		for (int i = 0; i < ITEMS_ON_MENU; i++) {
+			long cost = cost_per_menu_item[i];
+			grand_total += cost;
+		}
+		printf(
+			"The bill comes out to ¥%ld.%02d\n",
+			YUAN(grand_total), FEN(grand_total)
+		);
+	}
 
-    // 9. Teardown
-    if (rank == ROOT_PROCESS) {
-        // Only the root node has the full item and quantity arrays.
-        free(all_items);
-        free(all_quantities);
-    }
-    // All nodes have the segment arrays.
-    free(segment_items);
-    free(segment_quantities);
+	// 9. Teardown
+	if (rank == ROOT_PROCESS) {
+		// Only the root node has the full item and quantity arrays.
+		free(all_items);
+		free(all_quantities);
+	}
+	// All nodes have the segment arrays.
+	free(segment_items);
+	free(segment_quantities);
 
-    MPI_Finalize();
-    return 0;
+	MPI_Finalize();
+	return 0;
 }
 
 static void parse_menu(int *menu) {
-    FILE* f = fopen("menu.tsv", "r");
-    assert(f != NULL);
+	FILE* f = fopen("menu.tsv", "r");
+	assert(f != NULL);
 
-    int n_items = 0;
-    for (;;) {
-        int index;
-        // ignore the name; store the prices
-        int parsed = fscanf(f, "%d %*s %d", &index, &menu[n_items]);
-        if (parsed != 2) {
-            // No more items to parse.
-            break;
-        }
+	int n_items = 0;
+	for (;;) {
+		int index;
+		// ignore the name; store the prices
+		int parsed = fscanf(f, "%d %*s %d", &index, &menu[n_items]);
+		if (parsed != 2) {
+			// No more items to parse.
+			break;
+		}
 
-        n_items++;
-    }
-    assert(n_items == ITEMS_ON_MENU);
+		n_items++;
+	}
+	assert(n_items == ITEMS_ON_MENU);
 
-    fclose(f);
+	fclose(f);
 }
 
 static int parse_order(int ** restrict item, int ** restrict quantity) {
-    FILE* f = fopen("order.bin", "rb");
+	FILE* f = fopen("order.bin", "rb");
 
-    if (f == NULL) {
-        perror("Could not open order.bin");
-        fprintf(stderr, "Are you sure you ran ./make-order "
-                        "in the current working directory?\n");
-        exit(EXIT_FAILURE);
-    }
+	if (f == NULL) {
+		perror("Could not open order.bin");
+		fprintf(stderr, "Are you sure you ran ./make-order "
+				"in the current working directory?\n");
+		exit(EXIT_FAILURE);
+	}
 
-    fseek(f, 0, SEEK_END);
-    int file_size = ftell(f);
-    rewind(f);
+	fseek(f, 0, SEEK_END);
+	int file_size = ftell(f);
+	rewind(f);
 
-    int size_of_order = file_size / sizeof(int) / 2;
-    int *all_items = malloc(size_of_order * sizeof(int));
-    int *all_quantities = malloc(size_of_order * sizeof(int));
-    if ((all_items == NULL) || (all_quantities == NULL)) {
-        perror("could not allocate");
-        exit(EXIT_FAILURE);
-    }
+	int size_of_order = file_size / sizeof(int) / 2;
+	int *all_items = malloc(size_of_order * sizeof(int));
+	int *all_quantities = malloc(size_of_order * sizeof(int));
+	if ((all_items == NULL) || (all_quantities == NULL)) {
+		perror("could not allocate");
+		exit(EXIT_FAILURE);
+	}
 
-    // Note: I tried using stat + mmap and it was identical in performance to
-    // stdio. So don't bother optimizing this:
-    int num_items = fread(all_items, sizeof(int), size_of_order, f);
-    int num_quantities = fread(all_quantities, sizeof(int), size_of_order, f);
-    if ((num_items != size_of_order) || (num_quantities != size_of_order)) {
-        fprintf(stderr, "Error: Tried to read %d items from the order, "
-                        "but could only read %d items and %d quantities. "
-                        "Quiting!\n", size_of_order, num_items, num_quantities);
-        exit(EXIT_FAILURE);
-    }
-    fclose(f);
+	// Note: I tried using stat + mmap and it was identical in performance to
+	// stdio. So don't bother optimizing this:
+	int num_items = fread(all_items, sizeof(int), size_of_order, f);
+	int num_quantities = fread(all_quantities, sizeof(int), size_of_order, f);
+	if ((num_items != size_of_order) || (num_quantities != size_of_order)) {
+		fprintf(stderr, "Error: Tried to read %d items from the order, "
+				"but could only read %d items and %d quantities. "
+				"Quiting!\n", size_of_order, num_items, num_quantities);
+		exit(EXIT_FAILURE);
+	}
+	fclose(f);
 
-    // Pass the pointers back.
-    *item = all_items;
-    *quantity = all_quantities;
-    return size_of_order;
+	// Pass the pointers back.
+	*item = all_items;
+	*quantity = all_quantities;
+	return size_of_order;
 }
